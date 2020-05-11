d80e9eb184e78f8fdccffe712fdfd9519fa3c64b
Ceejaymar <martinezcj2@gmail.com>
2020-05-10 20:33:00 -0400

    Build card component

--- App.js
+++ App.js
@@ -1,19 +1,61 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text} from 'react-native';
+import styled from 'styled-components';
+import Card from './components/Card';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Container>
+      <TitleBar>
+        <Avatar source={require(`./assets/avatar-default.jpg`)} />
+        <Title>Welcome back, </Title>
+        <Name>Carlos</Name>
+      </TitleBar>
+      <Subtitle>Continue Learning</Subtitle>
+      <Card />
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.View`
+  flex: 1;
+  background-color: #F0F3F5;
+`;
+
+const TitleBar = styled.View`
+  width: 100%;
+  margin-top: 50px;
+  padding-left: 80px;
+`;
+
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  background-color: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const Title = styled.Text`
+  color: #b8bece;
+  font-size: 16px;
+  font-weight: 500;
+`;
+
+const Name = styled.Text`
+  color: #3c4560;
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-size: 15px;
+  font-weight: 600;
+  margin-left: 20px;
+  margin-top: 50px;
+  text-transform: uppercase;
+`;
